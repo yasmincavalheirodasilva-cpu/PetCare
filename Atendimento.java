828c0f681dc44e7db6d0fcd25a94c13fbc74af8a
Yasmin <yasmincavalheirodasilva@gmail.com>
2026-09-02 20:24:51 -0300

    Classe de atendimento corrigida

--- Atendimento.java
+++ Atendimento.java
@@ -1,36 +1,24 @@
-import java.time.LocalDateTime;
-
 public class Atendimento {
+
     private int idAtendimento;
-    private int idCliente;
     private int idPet;
     private int idFuncionario;
-    private String descricao;
-    private LocalDateTime dataAtendimento;
+    private String tipoAtendimento;
+    private double valor;
+    private String dataAtendimento;
 
-    public Atendimento(int idCliente, int idPet, int idFuncionario, String descricao, LocalDateTime dataAtendimento) {
-        this.idCliente = idCliente;
+    public Atendimento(int idPet, int idFuncionario, String tipoAtendimento, double valor, String dataAtendimento) {
         this.idPet = idPet;
         this.idFuncionario = idFuncionario;
-        this.descricao = descricao;
+        this.tipoAtendimento = tipoAtendimento;
+        this.valor = valor;
         this.dataAtendimento = dataAtendimento;
     }
 
-    public int getIdAtendimento() { return idAtendimento; }
-    public void setIdAtendimento(int idAtendimento) { this.idAtendimento = idAtendimento; }
-
-    public int getIdCliente() { return idCliente; }
-    public void setIdCliente(int idCliente) { this.idCliente = idCliente; }
-
-    public int getIdPet() { return idPet; }
-    public void setIdPet(int idPet) { this.idPet = idPet; }
-
-    public int getIdFuncionario() { return idFuncionario; }
-    public void setIdFuncionario(int idFuncionario) { this.idFuncionario = idFuncionario; }
-
-    public String getDescricao() { return descricao; }
-    public void setDescricao(String descricao) { this.descricao = descricao; }
-
-    public LocalDateTime getDataAtendimento() { return dataAtendimento; }
-    public void setDataAtendimento(LocalDateTime dataAtendimento) { this.dataAtendimento = dataAtendimento; }
+    public int getIdAtendimento() {return idAtendimento;}public void setIdAtendimento(int idAtendimento) {this.idAtendimento = idAtendimento;}
+    public int getIdPet() {return idPet;}public void setIdPet(int idPet) {this.idPet = idPet;}
+    public int getIdFuncionario() {return idFuncionario;}public void setIdFuncionario(int idFuncionario) {this.idFuncionario = idFuncionario;}
+    public String getTipoAtendimento() {return tipoAtendimento;}public void setTipoAtendimento(String tipoAtendimento) {this.tipoAtendimento = tipoAtendimento;}
+    public double getValor() {return valor;}public void setValor(double valor) {this.valor = valor;}
+    public String getDataAtendimento() {return dataAtendimento;}public void setDataAtendimento(String dataAtendimento) {this.dataAtendimento = dataAtendimento;}
 }
