003c61b5828485efdbcfd04d036dd34968fbae13
Yasmin <yasmincavalheirodasilva@gmail.com>
2026-09-02 21:03:31 -0300

    Classe funcionario corrigida

--- Funcionario.java
+++ Funcionario.java
@@ -1,9 +1,12 @@
 public class Funcionario {
+
+
     private int idFuncionario;
     private String nome;
     private String cpf;
     private String telefone;
     private String cargo;
+
 
     public Funcionario(String nome, String cpf, String telefone, String cargo) {
         this.nome = nome;
@@ -12,18 +15,9 @@ public class Funcionario {
         this.cargo = cargo;
     }
 
-    public int getIdFuncionario() {return idFuncionario; }
-    public void setIdFuncionario(int id_funcionario) { this.idFuncionario = id_funcionario; }
-
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-
-    public String getCpf() {return cpf;}
-    public void setCpf(String  cpf) { this.cpf = cpf; }
-
-    public String getTelefone() {return telefone; }
-    public void setTelefone(String telefone) {this.telefone = telefone; }
-
-    public String getCargo() {return cargo; }
-    public void setCargo(String cargo) {this.cargo = cargo; }
+    public int getIdFuncionario() {return idFuncionario;} public void setIdFuncionario(int idFuncionario) {this.idFuncionario = idFuncionario;}
+    public String getNome() {return nome;} public void setNome(String nome) {this.nome = nome;}
+    public String getCpf() {return cpf;} public void setCpf(String cpf) {this.cpf = cpf;}
+    public String getTelefone() {return telefone;} public void setTelefone(String telefone) {this.telefone = telefone;}
+    public String getCargo() {return cargo;} public void setCargo(String cargo) {this.cargo = cargo;}
 }
